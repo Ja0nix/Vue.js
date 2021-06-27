922a3b43ac443f71642c4bfafa9456478408cbf3
Ja0nix <62944305+Ja0nix@users.noreply.github.com>
2021-06-27 10:07:02 +0300

    Код калькулятора готов
    Возведение в степень и целочисленное деление добавлены

---
+++ first-project/src/components/Calculator.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>Calculator</h1>
+    <div class="display">
+       <input v-model="operand1"/>
+       <input v-model="operand2"/>
+       = {{result}}
+    </div>
+    <div class="keyboard">
+        <button v-on:click="result = operand1 + operand2">+</button>  
+        <button v-on:click="result = operand1 - operand2">-</button>  
+        <button @click="result = operand1 / operand2">/</button>  
+        <button @click="result = operand1 * operand2">*</button>  
+        <button @click="result = Math.pow(operand1, operand2)">В степень</button>  
+        <button @click="result = Math.floor(operand1/operand2)">Целочисленное деление</button>  
+    </div>
+  </div>
+</template>
+
+<script>
+ export default {
+   name: 'Calculator',
+   data(){
+       return {
+           operand1: 0,
+           operand2: 0,
+           result: 0,
+       }
+   }
+ }
+</script>
