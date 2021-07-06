2c60617f2627e57e9d4ee05ec4a4778c587e24a9
Ja0nix <62944305+Ja0nix@users.noreply.github.com>
2021-07-06 19:38:37 +0300

    поправлена логика

--- first-project/src/components/Calculator.vue
+++ first-project/src/components/Calculator.vue
@@ -18,8 +18,8 @@
       <input type="checkbox" id="checkbox" v-model="checked">
       <label for="checkbox">Отобразить экранную клавиатуру</label>
       <div class="cart-block" v-show="checked">
-        <button v-for="number in numbers" v-bind:key="number" @click="operand0 = number, selectedOperand()">{{number}}</button>
-        <button @click="selectedOperandDelete()">Удалить</button>
+        <button v-for="number in numbers" v-bind:key="number" @click="numValue = number, selectedOperand()">{{number}}</button>
+        <button @click="selectedOperandDelete()">Удалить последний символ</button>
         <br>
         <input type="radio" id="one" value="operand1" v-model="picked">
         <label for="one">operand1</label>
@@ -41,25 +41,31 @@
            result: 0,
            checked: false,
            picked: 0,
-           operand0: 0,
+           numValue: 0,
+           operand01: [],
+           operand02: [],
            numbers: [0,1,2,3,4,5,6,7,8,9],
        }
    },
    methods: {
      selectedOperand() {
        if(this.picked === "operand1") {
-         this.operand1 = this.operand0
+         this.operand01.push(this.numValue);
+         this.operand1 = +this.operand01.join('');
        }
        if(this.picked === "operand2") {
-         this.operand2 = this.operand0
+         this.operand02.push(this.numValue);
+         this.operand2 = +this.operand02.join('')
        }
      },
      selectedOperandDelete() {
        if(this.picked === "operand1") {
-         this.operand1 = 0
+         this.operand01.splice(-1,1);
+         this.operand1 = +this.operand01.join('');
        }
        if(this.picked === "operand2") {
-         this.operand2 = 0
+         this.operand02.splice(-1,1);
+         this.operand2 = +this.operand02.join('');
        }
      }
    }
